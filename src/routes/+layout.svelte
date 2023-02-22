508707b5c05ff7ec8181428bc14e100d0b175cda
colio <colioworks@gmail.com>
2023-02-22 12:33:08 -0600

    Init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import "../app.css";
+  </script>
+
+  <nav class="bg-black text-white">
+    <ul class="flex flex-row justify-evenly py-4">
+        <li class="text-2xl"><a href="/">Home</a></li>
+        <li class="text-2xl"><a href="/posts">Posts</a></li>
+    </ul>
+  </nav>
+  
+  <slot />
